8ee5569e28763f1e9d475d576df2c06332690bde
Walt Ostrander <waltostrander@gmail.com>
2023-02-14 09:53:48 -0600

    functions tests working, duo tests failing yet

--- functions.test.ts
+++ functions.test.ts
@@ -2,13 +2,13 @@ const { shuffleArray } = require("./utils");
 
 describe("shuffleArray should", () => {
   // CODE HERE
-  test("return an array with equal length", () => {
+  test("return an array with same length as argument", () => {
     const testArray = ["a", "b", "c"];
     const result = shuffleArray(testArray);
     expect(result.length).toEqual(testArray.length);
   });
 
-  test("return an array with same items", () => {
+  test("return an array with the same items", () => {
     const testArray = ["a", "b", "c"];
     const result = shuffleArray(testArray);
     expect(result).toContain("a");
